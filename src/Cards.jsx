8a9a37e141aa03f427e9f76f35ea66848a0f92c8
ymelki <67371192+ymelki@users.noreply.github.com>
2024-02-14 16:30:44 +0100

    ajout des cards

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardMeta } from "semantic-ui-react";
 
  
-function Cards({title}) { 
+function Cards({title, id}) { 
 
      return (  
                 <Card>
                     <CardContent>
-                    <Link to={`/service/${title}`} className="card-link">
+                    <Link to={`/service/${id}`} className="card-link">
                         <CardHeader>{title}</CardHeader>
                     </Link> 
                     </CardContent>
